2422205dd37f6a31a05e2e42c0d89c0fa0177c0f
ReaperMaga <reapermaga@gmail.com>
2022-05-22 03:40:43 +0200

    Fixed atropos not working on firefox and safari, improved about me text, removed horizontal padding from projects grid div

--- frontend/src/components/about/AboutMeProjects.tsx
+++ frontend/src/components/about/AboutMeProjects.tsx
@@ -11,7 +11,7 @@ const AboutMeProjects = () => {
                 wrapper="h2"
                 className="text-4xl uppercase text-zinc-300 font-code text-sky-300"
             />
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 px-2 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 w-full">
                 <AboutMeProjectCard title="MyAnimeApp" imageUrl="projects/myanimeapp.jpeg" url="https://imgur.com/a/1yy39n6">
                     <div className="py-px px-2 text-gray-300 bg-red-500 rounded-md text-md">Website</div>
                 </AboutMeProjectCard>
