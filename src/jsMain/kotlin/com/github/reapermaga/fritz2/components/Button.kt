cb6bdc0e08a85a7fa789110debb7ae88e29293b1
ReaperMaga <reapermaga@gmail.com>
2024-12-03 13:20:07 +0100

    feat: Improved button design

--- src/jsMain/kotlin/com/github/reapermaga/fritz2/components/Button.kt
+++ src/jsMain/kotlin/com/github/reapermaga/fritz2/components/Button.kt
@@ -9,7 +9,8 @@ enum class ButtonType(val classes: String) {
 }
 
 fun RenderContext.customButton(text: String, type : ButtonType = ButtonType.PRIMARY, onClick: () -> Unit = {}) {
-    button(joinClasses(type.classes, "transition text-white py-2 px-4 rounded")) {
+    button(joinClasses(type.classes, "transition text-white py-2 px-4 pr-5 rounded")) {
+        inlineStyle("box-shadow: inset 0.4px 1px 4px hsla(0, 0%, 50%, 0.8);")
         +text
         clicks handledBy {
             onClick()
