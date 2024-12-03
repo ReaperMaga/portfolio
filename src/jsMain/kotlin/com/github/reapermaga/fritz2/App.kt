b1288732825266eb8849f8d6fbbdea9f02df8838
ReaperMaga <reapermaga@gmail.com>
2024-12-03 13:25:27 +0100

    feat: Added grid pattern

--- src/jsMain/kotlin/com/github/reapermaga/fritz2/App.kt
+++ src/jsMain/kotlin/com/github/reapermaga/fritz2/App.kt
@@ -1,5 +1,6 @@
 package com.github.reapermaga.fritz2
 
+import com.github.reapermaga.fritz2.components.gridPattern
 import com.github.reapermaga.fritz2.components.header
 import com.github.reapermaga.fritz2.components.initializeScreen
 import com.github.reapermaga.fritz2.components.navbar
@@ -11,6 +12,7 @@ import dev.fritz2.core.render
 fun main() {
     render {
         initializeScreen()
+        gridPattern()
         div("w-screen min-h-screen bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,#0F110C_0%,#0C0E0A_100%)] flex flex-col inter-500 text-gray-300") {
             stars()
             div("w-8/12 mx-auto z-10") {
---
+++ src/jsMain/kotlin/com/github/reapermaga/fritz2/components/GridPattern.kt
@@ -0,0 +1,7 @@
+package com.github.reapermaga.fritz2.components
+
+import dev.fritz2.core.RenderContext
+
+fun RenderContext.gridPattern() {
+    div("grid-pattern absolute w-screen h-screen") {}
+}
