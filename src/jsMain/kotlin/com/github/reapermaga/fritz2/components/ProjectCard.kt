4edd77508e8130275927bb0d905f0844da5c9cab
ReaperMaga <reapermaga@gmail.com>
2024-12-03 12:26:38 +0100

    chore: Change border color on hover for project card

--- src/jsMain/kotlin/com/github/reapermaga/fritz2/components/ProjectCard.kt
+++ src/jsMain/kotlin/com/github/reapermaga/fritz2/components/ProjectCard.kt
@@ -7,7 +7,7 @@ import dev.fritz2.core.src
 import dev.fritz2.core.target
 
 fun RenderContext.projectCard(title: String, url: String, image: String, description: String) {
-    div("flex flex-col justify-between overflow-hidden w-full h-72 glass bg-background-400/60 border border-background-200 hover:-translate-y-1 transition") {
+    div("flex flex-col justify-between overflow-hidden w-full h-72 glass bg-background-400/60 border border-background-200 hover:border-background-50 hover:-translate-y-1 transition") {
         div("w-full flex justify-between px-5 py-6") {
             div("flex flex-col gap-3") {
                 h2("text-xl font-bold") {
