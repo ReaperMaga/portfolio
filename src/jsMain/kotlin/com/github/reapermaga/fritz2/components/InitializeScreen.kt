0c7a5d2cfaf9851cd4bda0cbbf25ded97a2c4481
ReaperMaga <reapermaga@gmail.com>
2024-12-04 12:07:41 +0100

    fix: Initialize screen

--- src/jsMain/kotlin/com/github/reapermaga/fritz2/components/InitializeScreen.kt
+++ src/jsMain/kotlin/com/github/reapermaga/fritz2/components/InitializeScreen.kt
@@ -12,14 +12,14 @@ fun RenderContext.initializeScreen() {
     val store = storeOf(false)
     div {
         transition(
-            enter = "transition delay-[3000ms] duration-500",
+            enter = "transition delay-[2500ms] duration-500",
             enterStart = "opacity-100 blur-none",
             enterEnd = "opacity-0 blur-lg",
             leave = "transition duration-500",
             leaveStart = "opacity-0 blur-lg",
             leaveEnd = "opacity-100 blur-none",
         )
-        className(store.data.onEach { waitForAnimation() }.map { if(it) "" else "hidden" }, "w-screen h-screen loader-background flex items-center justify-center absolute z-20",
+        className(store.data.onEach { waitForAnimation() }.map { if(it) "" else "hidden" }, "w-screen h-screen loader-background flex items-center justify-center z-20 fixed",
         )
         div("loader") {
             div("loader-inner") {
